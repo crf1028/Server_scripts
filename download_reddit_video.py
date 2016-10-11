dae38a541fc5eb6c2747a2e3a6daa8a5553cd310
crf1028 <crf901028@gmail.com>
2016-10-11 08:14:55 +0800

    rm

--- download_reddit_video.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from main_scripts import download_reddit_video
-
-
-download_reddit_video()
--- get_reddit_hot_info.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from main_scripts import get_reddit_hot_info
-
-
-get_reddit_hot_info()
